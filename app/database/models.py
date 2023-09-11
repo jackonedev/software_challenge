952a097884171f5019bc510ec6fcede347ca1969
unknown <jackone.action.software@gmail.com>
2023-09-11 16:59:06 -0300

    added table: User model for users table

--- app/database/models.py
+++ app/database/models.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, Integer, String
 from .database import Base
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
 
 class Post(Base):
     __tablename__ = "posts"
@@ -8,3 +10,11 @@ class Post(Base):
     author = Column(String)
     description = Column(String)
     my_numeric_field = Column(Integer)
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, nullable=False)
+    username = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('CURRENT_TIMESTAMP'))
