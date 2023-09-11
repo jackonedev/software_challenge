99f242c2ad40ef0d162879bc3c75f4717d7c29e6
unknown <jackone.action.software@gmail.com>
2023-09-11 20:43:30 -0300

    creating: testing module

--- app/tests/test_create_post.py
+++ app/tests/test_create_post.py
@@ -1,19 +1,10 @@
-from fastapi.testclient import TestClient
-from sqlalchemy.orm import sessionmaker
-from main import app
-from database.database import Base, engine
+from .database import client, session
 from database import models
 from schemas import posts
 
 
-def test_create_post():
-    # Crea una instancia del cliente de prueba
-    client = TestClient(app)
 
-    # Crea una sesión de base de datos temporal
-    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    Base.metadata.create_all(bind=engine)
-    db = TestingSessionLocal()
+def test_create_post(client, session):
 
     # Crea un objeto de prueba de tipo PostCreate
     post = posts.PostCreate(
@@ -25,7 +16,7 @@ def test_create_post():
 
     # Envía una solicitud POST al endpoint 'create_post'
     response = client.post("input/field_1", json=post.model_dump())
-
+    print(response.json())
     # Verifica que la respuesta tenga un código de estado 201
     assert response.status_code == 201
 
@@ -33,7 +24,7 @@ def test_create_post():
     assert "id" in response.json()
 
     # Verifica que el objeto de prueba haya sido agregado a la base de datos
-    db_post = db.query(models.Post).filter(models.Post.ID == response.json()["id"]).first()
+    db_post = session.query(models.Post).filter(models.Post.ID == response.json()["id"]).first()
     assert db_post is not None
 
     # Verifica que el campo 'field_1' del objeto de prueba haya sido convertido a mayúsculas
