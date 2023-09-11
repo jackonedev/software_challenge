400c2e19437205f68e1687062bb6461721792474
unknown <jackone.action.software@gmail.com>
2023-09-11 15:02:06 -0300

    updated: /input api, with .upper() method included

--- app/api/input.py
+++ app/api/input.py
@@ -1,5 +1,8 @@
+from enum import Enum
 from fastapi import status, HTTPException, Depends, APIRouter
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
+
 
 from database.database import get_db
 from database import models
@@ -12,6 +15,29 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-async def root():
-    return {"data": "Hola mundo desde input"}
+class QueryType(str, Enum):
+    field_1 = "field_1"
+    author = "author"
+    description = "description"
+
+
+@router.post("/{query}")
+async def create_post(query: str, post: posts.PostCreate, db: Session = Depends(get_db)):
+    try:
+        query_type = QueryType(query)
+    except ValueError:
+        return JSONResponse(status_code=400, content={"error": f"{query} no es un campo válido para convertir a mayúsculas"})
+
+    if query_type == QueryType.field_1:
+        post.field_1 = post.field_1.upper()
+    elif query_type == QueryType.author:
+        post.author = post.author.upper()
+    elif query_type == QueryType.description:
+        post.description = post.description.upper()
+
+    db_post = models.Post(**post.model_dump())
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+
+    return {"id": db_post.id}
