f8c0912470cd0e3293e6e31a61ed374e2fe12f3f
unknown <jackone.action.software@gmail.com>
2023-09-11 13:14:44 -0300

    finished: sqlite config

--- app/database/database.py
+++ app/database/database.py
@@ -1,15 +1,6 @@
-# from functools import lru_cache
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-# from ..utils import config
 
-
-# @lru_cache()
-# def get_settings():
-#     return config.Settings()
-
-
-# settings = get_settings()
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
 
